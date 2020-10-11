a2ef257e7b344e77ebc918c492ef5f84404bf367
MilhamAt <M477ashiddiq@gmail.com>
2020-10-11 22:01:30 +0700

    image

--- Controller/MainController.swift
+++ Controller/MainController.swift
@@ -6,11 +6,17 @@
 //
 
 import UIKit
+import SnapKit
 
 class MainController: UIViewController {
+    
+    private var mainView: MainView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.mainView = MainView(frame: self.view.frame)
+        self.view = self.mainView
         
     }
 
---
+++ View/MainView.swift
@@ -0,0 +1,44 @@
+//
+//  Main.swift
+//  NoStoryBoard
+//
+//  Created by Muhammad Ilham Ashiddiq Tresnawan on 11/10/20.
+//
+
+import UIKit
+import SnapKit
+
+class MainView: UIView {
+    
+    let foodImage: UIImageView = {
+        let image = UIImageView()
+        image.image = UIImage(named: "sate")
+        image.contentMode = .scaleAspectFit
+        
+        return image
+    }()
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        self.setupView()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupView() {
+        self.backgroundColor = .white
+        
+        self.addSubview(foodImage)
+        
+        self.foodImage.snp.makeConstraints { (make) in
+            make.centerX.equalTo(self.safeAreaLayoutGuide)
+            make.centerY.equalTo(self.safeAreaLayoutGuide)
+            make.height.equalTo(100)
+            make.width.equalTo(100)
+        }
+    }
+}
