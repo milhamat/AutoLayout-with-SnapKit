1322240cd7b37342e8261b25a8a1d482a3bcbe09
MilhamAt <M477ashiddiq@gmail.com>
2020-10-11 21:16:55 +0700

    Initial Commit

---
+++ Controller/MainController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NoStoryBoard
+//
+//  Created by Muhammad Ilham Ashiddiq Tresnawan on 11/10/20.
+//
+
+import UIKit
+
+class MainController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,13 @@
+
+platform :ios, '10.0'
+
+target 'NoStoryBoard' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for NoStoryBoard
+
+  pod 'SnapKit'
+
+
+end
